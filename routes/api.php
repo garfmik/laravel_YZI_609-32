783ecd687129611387e5db62a3dbc1839a562c60
garfmik <zulfiaaganova@gmail.com>
2026-02-26 19:49:36 +0500

    Установка Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,30 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RestaurantControllerApi;
 use App\Http\Controllers\ReviewControllerApi;
 use App\Http\Controllers\UserControllerApi;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::get('/restaurants', [RestaurantControllerApi::class, 'index']);
+
 Route::get('/restaurants/{id}', [RestaurantControllerApi::class, 'show']);
 
-Route::get('/restaurants/{id}/reviews', [ReviewControllerApi::class, 'index']);
+
 Route::get('/reviews/{id}', [ReviewControllerApi::class, 'show']);
 
 
 Route::get('/users/{id}', [UserControllerApi::class, 'show']);
+
+
+
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/restaurants/{id}/reviews', [ReviewControllerApi::class, 'index']);
+    Route::get('/user',function (Request $request){
+        return $request->user();
+    });
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
